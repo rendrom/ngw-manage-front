8c6f6103553c3e46ddd6b2c193c4592c3700dde9
Artemiy <rendrom@gmail.com>
2021-02-24 23:53:38 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,25 @@
+# ngw-manage-front
+
+## Project setup
+
+```bash
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```bash
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```bash
+npm run build
+```
+
+### Lints and fixes files
+
+```bash
+npm run lint
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <a-button type="primary"> Primary </a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {}
+});
+</script>
