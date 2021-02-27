f7d74a766ec8a9eb8a5308beca7e075001cf1b53
Artemiy <rendrom@gmail.com>
2021-02-27 21:50:54 +0800

    wip: auth form test

--- src/services/ngwService.ts
+++ src/services/ngwService.ts
@@ -1,5 +1,3 @@
 import NgwConnector, { NgwConnectorOptions } from '@nextgis/ngw-connector';
 
-export function createConnector(options: NgwConnectorOptions): NgwConnector {
-  return NgwConnector.create(options);
-}
+export const connector = new NgwConnector({});
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,12 @@
-<template></template>
+<template>
+  <auth-form></auth-form>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AuthForm from "../components/Auth.vue";
 
 export default defineComponent({
-  //
+  components: { AuthForm },
 });
 </script>
